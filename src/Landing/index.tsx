4ccefefc91a83928a3e227fc724537d5e21afe17
Christian Cuneo <ccuneo@cpat.com>
2022-01-31 23:31:13 -0600

    add divs

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -2,6 +2,16 @@ import landing from './landing'
 
 export default function Landing() {
     
-    return <div className="landing">hello</div>
+    return (
+      <div className="landing">
+        <div className="Div 1"></div>
+        <div className="Div 2"></div>
+        <div className="Div 3"></div>
+        <div className="Div 4"></div>
+        <div className="Div 5"></div>
+        <div className="Div 6"></div>
+        <div className="Div 7"></div>
+      </div>
+    );
 
 }
