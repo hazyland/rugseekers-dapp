3c3d6fdc471da242cd7dd20e88867927506f0a46
Christian Cuneo <ccuneo@cpat.com>
2022-01-31 23:52:13 -0600

    html & component structure

--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -1,9 +1,22 @@
 import landing from './landing'
+import LoginPanel from './loginPanel';
+import ProjectCard from './projectCard';
+import RoadmapCard from './roadmapCard';
+import TokenomicsPanel from './tokenomicsPanel';
+
+
 
 export default function Landing() {
     
     return (
       <div className="landing">
+          <div className="who">Who are Rugseekers</div>
+          <div className="featuredIn">Bloomberg</div>
+          <div className="bounty">Yar Bounty Program<LoginPanel/></div>
+          <div className="tokenomics">Tokenomics<TokenomicsPanel/></div>
+          <div className="vettedProjects">Vetted Projects<ProjectCard/></div>
+          <div className="roadMap">Raodmap<RoadmapCard/></div>
+
       </div>
     );
 
