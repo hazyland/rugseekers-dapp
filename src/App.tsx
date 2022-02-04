6590f5cd185203cb377eeee5f608a8548acd033a
Christian Cuneo <ccuneo@cpat.com>
2022-02-03 22:29:30 -0600

    working with app state and adaptive css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as BrowserRouterProvider, Route, Routes } from "react-router-dom";
 import { useAppProviderDispatch, useAppProvider } from "./contexts/App/AppProvider";
+
 import Dapp from "./Dapp";
 import Landing from "./Landing";
 import Navigation from "./Navigation"
@@ -12,29 +13,55 @@ import AppProvider from "./contexts/App/AppProvider"
 
 import { useState, useEffect } from "react";
 
+  
+
+
 
 function App() {
-  
   const AppState = useAppProvider()
   const AppDispatch = useAppProviderDispatch()
 
+  function useWindowDimensions() {
+    function getWindowDimensions() {
+      const { innerWidth: width, innerHeight: height } = window;
+      return {
+        innerWidth:width,
+        innerHeight:height,
+      };
+    }
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+    useEffect(() => {
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+  
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }, []);
+  
+    return windowDimensions;
+  }
 
+  let windowDimensions = useWindowDimensions()
+  
 
-    return (
-      <div className={AppState.appDimensions.innerWidth <= 500 ? "app-mobile" : "app"}>
-        <BrowserRouterProvider>
-          <div className="column-2">
-            <Navigation></Navigation>
-            <Web3Provider>
-              <Routes>
-                <Route path="/dapp" element={<Dapp />} />
-                <Route path="/landing" element={<Landing />} />
-              </Routes>
-            </Web3Provider>
-          </div>
-        </BrowserRouterProvider>
-      </div>
-    );
+  AppDispatch({ type: "updateDimensions", payload: windowDimensions});
+  
+  return (
+    <div className={AppState.appDimensions.innerWidth <= 500 ? "app-mobile" : "app"}>
+      <BrowserRouterProvider>
+        <div className="column-2">
+          <Navigation></Navigation>
+          <Web3Provider>
+            <Routes>
+              <Route path="/dapp" element={<Dapp />} />
+              <Route path="/landing" element={<Landing />} />
+            </Routes>
+          </Web3Provider>
+        </div>
+      </BrowserRouterProvider>
+    </div>
+  );
 }
 
 export default App;
--- src/Landing/index.tsx
+++ src/Landing/index.tsx
@@ -6,50 +6,52 @@ import TokenomicsPanel from './tokenomicsPanel';
 import KeyPointsSlide from './keyPointsSlide';
 import Social from './social'
 
+import { useAppProvider } from '../contexts/App/AppProvider';
+
+
+
 import bountyIMG from '../assets/pirate.png'
 
 
 function Landing() {
-    
+    const AppState = useAppProvider()
+
     return (
       <div className="landing">
-            <div className="landing-row row-1 dark-gray-background">
-                <div className="landing-row-social-container" >
-                    <div className="col-2">
-                            <h3>
-                            Worlds first project to pay a "bounty" for tips the lead to stopping corruption.
-                            </h3>
-                        <div className="landing-header-1">Who are RugSeekers?</div>
-                        <div>We are a professional, diligent, and experienced team committed to helping eliminate fraud and corruption from the crypto space. The RugSeeker team was born out of our own experience with loss and is dedicated to rid investors of that very thing.</div>               
-                    </div>
-                    <div>
-                    <Social/>
-                    </div>
-                </div>
-                <KeyPointsSlide />
-                
+        <div className="landing-row row-1 dark-gray-background">
+          <div className={AppState.appDimensions.innerWidth <= 500 ? "landing-row-1-mobile" : "landing-row-1"}>
+              <Social />
+            <div className={AppState.appDimensions.innerWidth <= 500 ? "row-2" : "col-2"}>
+              <h3>Worlds first project to pay a "bounty" for tips the lead to stopping corruption.</h3>
+              <div className="landing-header-1">Who are RugSeekers?</div>
+              <div>We are a professional, diligent, and experienced team committed to helping eliminate fraud and corruption from the crypto space. The RugSeeker team was born out of our own experience with loss and is dedicated to rid investors of that very thing.</div>
             </div>
-            <div className="landing-row dark-gray-background">
-                <div className="landing-header-1">RugSeekers has been featured in</div>
+            <div>
             </div>
-            <div className="landing-row dark-gray-background">
-                <img src={bountyIMG} height="100" width="100"></img>
-                <div className="landing-header-1">Yar Bounty Program</div>
-                <LoginPanel />
-            </div>
-            <div className="landing-row gray-background">
-                <div className="landing-header-1">Tokenomics</div>
-                <TokenomicsPanel />
-            </div>
-            <div className="landing-row dark-gray-background">
-                <div className="landing-header-1">Vetted Projects</div>
-                <ProjectCard />
-            </div>
-            <div className="landing-row gray-background">
-                <div className="landing-header-1">Raodmap</div>
-                <RoadmapCard />
-            </div>
+          </div>
+          <KeyPointsSlide />
         </div>
+        <div className="landing-row dark-gray-background">
+          <div className="landing-header-1">RugSeekers has been featured in</div>
+        </div>
+        <div className="landing-row dark-gray-background">
+          <img src={bountyIMG} height="100" width="100"></img>
+          <div className="landing-header-1">Yar Bounty Program</div>
+          <LoginPanel />
+        </div>
+        <div className="landing-row gray-background">
+          <div className="landing-header-1">Tokenomics</div>
+          <TokenomicsPanel />
+        </div>
+        <div className="landing-row dark-gray-background">
+          <div className="landing-header-1">Vetted Projects</div>
+          <ProjectCard />
+        </div>
+        <div className="landing-row gray-background">
+          <div className="landing-header-1">Raodmap</div>
+          <RoadmapCard />
+        </div>
+      </div>
     );
 
 }
