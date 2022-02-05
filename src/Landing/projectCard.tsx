2d7c1856b065e0893116287747b682d98ea12c5f
Christian Cuneo <ccuneo@cpat.com>
2022-02-05 13:21:19 -0600

    adding map for vetted projects

--- src/Landing/projectCard.tsx
+++ src/Landing/projectCard.tsx
@@ -7,38 +7,25 @@ export default function ProjectCard() {
 
   const vettedProjects = [{projectName:"Project Name Here", rating:93.5},{projectName:"Project Name Here", rating:93.5},{projectName:"Project Name Here", rating:93.5},{projectName:"Project Name Here", rating:93.5},{projectName:"Project Name Here", rating:93.5},{projectName:"Project Name Here", rating:93.5},{projectName:"Project Name Here", rating:93.5}]
   
+
+  function vettedProjectsMap() {
+
+    return (
+      <div className="projectCard">
+        <div>
+          <img src={project} height="80" width="80"></img>
+        </div>
+        <div>Project Name Goes Here</div>
+        <div className="score">93.5</div>
+      </div>
+    );
+  }
+
+
+
   return (
     <div className="landingRow6">
-        <div className="projectCard">
-          <div><img src={project} height="80" width="80"></img></div>
-          <div>Project Name Goes Here</div>
-          <div className="score">93.5</div>
-        </div>
-        <div className="projectCard">
-          <div><img src={project} height="80" width="80"></img></div>
-          <div>Project Name Goes Here</div>
-          <div className="score">93.5</div>
-        </div>
-        <div className="projectCard">
-          <div><img src={project} height="80" width="80"></img></div>
-          <div>Project Name Goes Here</div>
-          <div className="score">93.5</div>
-        </div>
-        <div className="projectCard">
-          <div><img src={project} height="80" width="80"></img></div>
-          <div>Project Name Goes Here</div>
-          <div className="score">93.5</div>
-        </div>
-        <div className="projectCard">
-          <div><img src={project} height="80" width="80"></img></div>
-          <div>Project Name Goes Here</div>
-          <div className="score">93.5</div>
-        </div>
-        <div className="projectCard">
-          <div><img src={project} height="80" width="80"></img></div>
-          <div>Project Name Goes Here</div>
-          <div className="score">93.5</div>
-        </div>
+      {vettedProjects.map(vettedProjectsMap)}
     </div>
   );
 }
