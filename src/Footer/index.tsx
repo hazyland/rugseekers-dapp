a61eb412bce1010bfcea54ca369e556467a87466
Christian Cuneo <ccuneo@cpat.com>
2022-02-08 22:25:47 -0600

    update footer

--- src/Footer/index.tsx
+++ src/Footer/index.tsx
@@ -11,6 +11,7 @@ import twiter from "../assets/twiter.png";
 import telegram from "../assets/telegram.png";
 import facebook from "../assets/facebook.png";
 import discord from "../assets/discord.png";
+import { relative } from "path/posix";
 
 function Links() {
 
@@ -23,56 +24,61 @@ function Links() {
 }
 
 function Footer() {
-
-
-  function hamburgerMenu() {
-    return <img src={hamburger} height="20" width="25"></img>;
+    
+    
+    function hamburgerMenu() {
+        return <img src={hamburger} height="20" width="25"></img>;
   }
 
   return (
-    <div className="footer" style={{ padding: "5px" }}>
-      <div style={{ display: "flex", justifyContent: "space-around" }}>
-        <div className="quick-links" style={{ display: "flex", flexDirection: "column", justifyContent: "space-around", marginTop: "5px" }}>
-          <h4>Quick Links</h4>
-          <div style={{display: "flex", fontSize:"12px"}}>
-              <div style={{display: "flex",margin:"5px", flexDirection:"column"}}>
-                <a>Whitepaper</a>
-                <a>Roadmap</a>
-                <a>Tokenomics</a>
-                <a>Reflection</a>
-              </div>
-              <div style={{display: "flex",margin:"5px", flexDirection:"column"}}>
-                <a>Vetted Projects</a>
-                <a>Online Store</a>
-                <a>How to Buy</a>
-                <a>Buy Crypto</a>
-              </div>
+      
+      
+      <div className="copyright">
+    <div className="footer" style={{ padding: "5px", display: "flex", justifyContent: "space-around" }}>
+      {/* <div style={{ display: "flex", justifyContent: "space-around" }}> */}
+      <div className="quick-links" style={{ display: "flex", flexDirection: "column", justifyContent: "space-around", marginTop: "5px" }}>
+        <h4>Quick Links</h4>
+        <div style={{ display: "flex", fontSize: "12px", justifyContent:"center"}}>
+          <div style={{ display: "flex", margin: "5px", flexDirection: "column" }}>
+            <a>Whitepaper</a>
+            <a>Roadmap</a>
+            <a>Tokenomics</a>
+            <a>Reflection</a>
+          </div>
+          <div style={{ display: "flex", margin: "5px", flexDirection: "column" }}>
+            <a>Vetted Projects</a>
+            <a>Online Store</a>
+            <a>How to Buy</a>
+            <a>Buy Crypto</a>
           </div>
         </div>
-
-        <div>
-          <h4>Contact Us</h4>
-          <a style={{ color: "orange" }}>RugSeekers@gmail.com</a>
-        </div>
-        <div>
-          <h4>Social Links</h4>
-          {/* <div className={"socialWidget-container" + "-mobile"}> */}
-            <div style={{display:"flex", justifyContent: "space-between"}}>
-                <img src={twiter}></img>
-                <img src={telegram}></img>
-                <img src={discord}></img>
-                <img src={facebook}></img>
-              {/* <div className="social-icon-container"> */}
-              {/* </div> */}
-
-
-
-            </div>
-          </div>
-        {/* </div> */}
       </div>
-      <p style={{ textAlign: "center" }}>Copyright Rugseekers 2022 - All Rights Reserved</p>
+
+      <div>
+        <h4>Contact Us</h4>
+        <a style={{ color: "orange" }}>RugSeekers@gmail.com</a>
+      </div>
+
+      
+      <div>
+        <h4>Social Links</h4>
+        {/* <div className={"socialWidget-container" + "-mobile"}> */}
+        <div style={{ display: "flex", justifyContent: "space-around" }}>
+          <img src={twiter}></img>
+          <img src={telegram}></img>
+          <img src={discord}></img>
+          <img src={facebook}></img>
+          {/* <div className="social-icon-container"> */}
+          {/* </div> */}
+        </div>
+      </div>
+      {/* </div> */}
+      {/* </div> */}
     </div>
+      <p style={{textAlign: "center"}}>Copyright Rugseekers 2022 - All Rights Reserved</p>
+
+      </div>
+
   );
 }
 
